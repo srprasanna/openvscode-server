327d70b800b6dddd300d8d9e8010bb9bd2b648c5
Prasanna Raghu <prasanna@beehyv.com>
2023-04-23 18:10:42 +0530

    websocke

--- src/vs/platform/remote/browser/browserSocketFactory.ts
+++ src/vs/platform/remote/browser/browserSocketFactory.ts
@@ -274,8 +274,8 @@ export class BrowserSocketFactory implements ISocketFactory {
 
 	connect(host: string, port: number, path: string, query: string, debugLabel: string, callback: IConnectCallback): void {
 		const webSocketSchema = (/^https:/.test(window.location.href) ? 'wss' : 'ws');
-		const socket = this._webSocketFactory.create(`${webSocketSchema}://${(/:/.test(host) && !/\[/.test(host)) ? `[${host}]` : host}:${port}${path}?${query}&skipWebSocketFrames=false`, debugLabel);
 		path = (window.location.pathname + "/" + path).replace(/\/\/+/g, "/")
+		const socket = this._webSocketFactory.create(`${webSocketSchema}://${(/:/.test(host) && !/\[/.test(host)) ? `[${host}]` : host}:${port}${path}?${query}&skipWebSocketFrames=false`, debugLabel);
 		const errorListener = socket.onError((err) => callback(err, undefined));
 		socket.onOpen(() => {
 			errorListener.dispose();
